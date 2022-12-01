d31c4b99c381b16bf8de6ea27706ecc65b81d642
Hemanth Reddy <UlashiHemanth.Reddy@unisys.com>
2022-12-01 12:39:00 +0530

    ddd

--- Jenkinsfile
+++ Jenkinsfile
@@ -20,7 +20,7 @@ pipeline {
         stage('Execute_Shell') {
             steps {
                 sh "jq '.[]' input-x.json"
-                sh "bash -f main.sh"
+                sh "bash -f main.sh ${params.CHOICE}"
 
             }
         }
--- main.sh
+++ main.sh
@@ -4,19 +4,21 @@ b=10
 dev=0
 it=1
 prod=2
-echo " xxxxxxxxxxxx   ${params.CHOICE}"
+echo "env: $1"
+echo xxxxxxxxxxxx
+echo "$1"
 #d=20
 #b=$(jq .env[$dev].${params}.memory input-x.json)
 #echo "$b"
 
 #Check one
-if [ ${params.CHOICE}  -ge $b ]
+if [ ${it}  -ge $b ]
 then
     echo "a '${params.CHOICE}' is greater than equal to b"
 fi
   
 #Check two
-if [ $a -le $b ]
+if [ $b -le $b ]
 then
     echo "a is less than equal to b"
 fi
